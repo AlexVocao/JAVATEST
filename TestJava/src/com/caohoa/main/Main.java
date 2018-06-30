3f16b6b4d34b23e86dfb5d4cd259362605078cda
hoa <mark.vo@asiapos.com.sg>
2018-06-30 12:59:56 +0700

    version 2

--- TestJava/src/com/caohoa/main/Main.java
+++ TestJava/src/com/caohoa/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		StringBuilder builderJson = new StringBuilder();
 		
-		builderJson.append("{ \"age\": 10");
+		builderJson.append("{ \"age\": 11");
 		builderJson.append(",\"name\":\"hung\"}");
 		
 		//Person person = new Person(10,"hung");
